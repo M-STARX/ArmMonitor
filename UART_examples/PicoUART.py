2b49dcbb620aba31948c91b6960aea5f0d333567
Granto1 <grantoyt@gmail.com>
2026-03-15 15:56:59 -0400

    Some minor changes

--- UART_examples/PicoUART.py
+++ UART_examples/PicoUART.py
@@ -1,8 +1,10 @@
 from machine import UART, Pin
 import time
 
+# using UART0, which is tx pins 0 and 1
 uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
+# testing counter
 count = 0
 while True:
     message = f"This was sent from the pico {count}\n"
@@ -10,8 +12,8 @@ while True:
     print("Sent:", message.strip())  
     count += 1
     time.sleep(1)
-    #while uart.in_waiting < 0:
         
+    # check if we have UART input
     if uart.any():
         mes = uart.readline().decode('utf-8').strip()
         print("Received:", mes)
